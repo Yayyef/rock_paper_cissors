c95722f56e4378a97cefb6fc35eac2052add8632
Yayyef <chassang.xavier@gmail.com>
2020-08-31 16:48:42 +0200

    test git

---
+++ script.js
@@ -0,0 +1,16 @@
+computerPlay = () => {
+    let computerChoice;
+    randomNum = Math.ceil(Math.random()*30);   
+    if (randomNum <= 10) {
+        computerChoice = "Rock!";
+    } if (10 < randomNum <= 20) {
+        computerChoice = "Paper!"
+    } else {
+        computerChoice = "Cissors!"
+    }
+    
+    console.log(computerChoice);
+    return randomNum;
+};
+
+computerPlay();
