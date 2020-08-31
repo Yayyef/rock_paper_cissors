b3414f8ba89cb01838dd54ab998142199b4ec5e3
Yayyef <chassang.xavier@gmail.com>
2020-08-31 17:59:57 +0200

    ca marche pas mais ca va le faire

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-computerPlay = () => {
+/* computerPlay = () => {
     let computerChoice;
     randomNum = Math.ceil(Math.random()*30);   
     if (randomNum <= 10) {
         computerChoice = "Rock!";
-    } if (10 < randomNum <= 20) {
+    } else if (10 < randomNum && randomNum <= 20) {
         computerChoice = "Paper!"
     } else {
         computerChoice = "Cissors!"
@@ -11,9 +11,41 @@ computerPlay = () => {
     
     console.log(computerChoice);
     return randomNum;
+}; */
+
+let computerChoice;
+let userChoice;
+
+function computerPlay() {
+    let computerChoice;
+    randomNum = Math.ceil(Math.random()*30);
+    computerChoice = (randomNum <= 10) ? "Rock!"
+                : (10 < randomNum && randomNum <= 20) ? "Paper!"
+                : "Cissors!"
+                    
+    
+    return computerChoice;
 };
 
-// un commentaire histoire de modifier eu la c'est mieux
-
+function shiFuMi(userChoice, computerChoice) {
+    let outcome;
+    
+    if (userChoice = "rock") {
+        outcome = (computerChoice == "Rock!") ? "Draw!"
+                : (computerChoice == "Paper!") ? "You win!"
+                : "You loose!"
+                return outcome;
+    } else if (userChoice = "paper") {
+        outcome = (computerChoice == "Rock!") ? "You win!"
+                : (computerChoice == "Paper!") ? "Draw!"
+                : "You loose!"
+                return outcome;
+    } else {
+        outcome = (computerChoice == "Rock!") ? "You loose!"
+                : (computerChoice == "Paper!") ? "You win!"
+                : "Draw!"
+                return outcome;
+    };           
+    }
 
 computerPlay();
