9ba51e4d1f037369b41bcc3124e3e4e79acf1853
Yayyef <chassang.xavier@gmail.com>
2020-09-01 20:29:31 +0200

    Ca marche! Utiliser des ? operators par parcimonie

--- script.js
+++ script.js
@@ -1,51 +1,86 @@
-/* computerPlay = () => {
-    let computerChoice;
-    randomNum = Math.ceil(Math.random()*30);   
-    if (randomNum <= 10) {
-        computerChoice = "Rock!";
-    } else if (10 < randomNum && randomNum <= 20) {
-        computerChoice = "Paper!"
-    } else {
-        computerChoice = "Cissors!"
-    }
-    
-    console.log(computerChoice);
-    return randomNum;
-}; */
-
-let computerChoice;
-let userChoice;
+let playerSelection = "rock";
+let computerSelection; 
+let userScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     let computerChoice;
     randomNum = Math.ceil(Math.random()*30);
     computerChoice = (randomNum <= 10) ? "Rock!"
                 : (10 < randomNum && randomNum <= 20) ? "Paper!"
-                : "Cissors!"
-                    
-    
+                : "Scissors!"
+                     
     return computerChoice;
 };
 
-function shiFuMi(userChoice, computerChoice) {
-    let outcome;
-    
-    if (userChoice = "rock") {
-        outcome = (computerChoice == "Rock!") ? "Draw!"
-                : (computerChoice == "Paper!") ? "You win!"
-                : "You loose!"
-                return outcome;
-    } else if (userChoice = "paper") {
-        outcome = (computerChoice == "Rock!") ? "You win!"
-                : (computerChoice == "Paper!") ? "Draw!"
-                : "You loose!"
-                return outcome;
-    } else {
-        outcome = (computerChoice == "Rock!") ? "You loose!"
-                : (computerChoice == "Paper!") ? "You win!"
-                : "Draw!"
-                return outcome;
-    };           
-    }
+function playRound(playerSelection, computerSelection) {
+    playerSelection  = prompt("Rock, paper, scissors!");
+    computerSelection = computerPlay();
+    let result;
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "Rock!") {
+            result = "Draw!";
+        } else if (computerSelection === "Paper!") {
+            computerScore += 1;
+            result = "You lose! Paper beats rock.";
+            
+        } else if (computerSelection === "Scissors!") {
+            userScore += 1;
+            result = "You win! Rock beats scissors.";
+            
+        } else {
+            result = "Wooops";
+        }
+        
+    } else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "Rock!") {
+            userScore += 1;
+            result = "You win! Paper beats rock.";
+            
+        } else if (computerSelection === "Paper!") {
+            result = "Draw!";
+        } else if (computerSelection === "Scissors!") {
+            computerScore += 1;
+            result = "You lose! Scissors beats paper.";
+            
+        } else {
+            result = "Wooops";
+        }
 
-computerPlay();
+    } else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "Rock!") {
+            computerScore += 1;
+            result = "You lose! Rock beats scissors.";
+            
+        } else if (computerSelection === "Paper!") {
+            userScore += 1;
+            result = "You win! Scissors beats paper.";
+            
+        } else if (computerSelection === "Scissors!") {
+            result = "Draw!";
+        } else {
+            result = "Wooops";
+        }
+    } else {
+        result = "You have to choose either rock, paper or scissors."
+    }
+    
+    return result;
+}
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+                      
+        let roundOutcome = playRound(playerSelection, computerSelection);
+        alert("Round " + i + ":\n" + roundOutcome);
+        console.log(userScore, computerScore);
+        // mettre ici un conditionnel pour augmenter ou diminuer user score?        
+        
+    };   
+    if (userScore > computerScore) { alert(userScore + " to " + computerScore + "\n" + "Victory!") }
+    else if (userScore < computerScore) { alert(userScore + " to " + computerScore + "\n" + "Better luck next time.")}
+    else { alert(userScore + " to " + computerScore + "\n" + "Tight game!") }
+}
+
+game();
+
